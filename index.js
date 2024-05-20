13ecb8d88a9edf4de279c6f1e39a93acde8b8c7b
hammadsadi <hammad.sadi@yahoo.com>
2024-05-20 16:53:56 +0600

    Added Single Room Route

--- index.js
+++ index.js
@@ -90,6 +90,12 @@ async function run() {
       const result = await roomCollection.find().toArray();
       res.send(result);
     });
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
